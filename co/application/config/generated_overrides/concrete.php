758d78750eea436e3bb0f633cd6bc98997ba6e29
125nciprusl <mcprus@o2.pl>
2015-09-28 21:11:48 +0200

    ...

---
+++ co/application/config/generated_overrides/concrete.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-06-01T20:39:22+02:00
+ *
+ * @item      misc.seen_introduction
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'site' => 'koszykowka',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1433183937,
+        'latest_version' => '5.7.3.1',
+        'seen_introduction' => true
+    )
+);
---
+++ co/application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/40d7c8ec25b53c62/6a5aee3d509fc711.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_8149b4fee226fd4f8f68a2026ed7faa1_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1443886935;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1443463869;
